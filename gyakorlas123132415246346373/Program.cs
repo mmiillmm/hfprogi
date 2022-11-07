03511bf65eea142ee63c5cb5a4aaa74eccef45b2
Makai Kristof <kmakai520@gmail.com>
2022-11-07 08:26:26 +0100

    Add project files.

---
+++ gyakorlas123132415246346373/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlas123132415246346373
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adatok = "10;5;7";
+
+            //foreach (var betu in adatok)
+            //{
+            //    if (betu == ';')
+            //    {
+            //        Console.WriteLine();
+            //    }
+
+            //    else
+            //    {
+            //        Console.Write(betu);
+            //    }
+            //}
+
+
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                if (adatok[i] == ';')
+                {
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.Write(adatok[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
